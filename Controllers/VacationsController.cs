02806c4e6be1aa4984d1db87dd0f149b6b90bed1
Amjad Alnahas <Amjad.Alnahas@plan-b-gmbh.com>
2025-06-10 15:19:52 +0200

    Add project files.

---
+++ Controllers/VacationsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DaprZeebeSample.Controllers;
+
+[ApiController]
+public class VacationsController : ControllerBase
+{
+    [HttpPost("dapr-zeebe-sample-save-data")]
+    public async Task<IActionResult> HandleJob()
+    {
+        using var reader = new StreamReader(Request.Body);
+        var body = await reader.ReadToEndAsync();
+
+        var result = new
+        {
+            variables = new
+            {
+                approved = true,
+                comment = "Handled by Dapr .NET controller"
+            }
+        };
+
+        return Ok(result);
+    }
+}
